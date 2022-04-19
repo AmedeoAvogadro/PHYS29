aa87362df7617e28cc19f0308c6df46a38fd3def
Yizhi Jiao <yizhi_jiao@ucsb.edu>
2022-04-19 14:33:33 -0700

    Homework 3

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import numpy as np
 
 
+def fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibo(n-1)+fibo(n-2)
+
+
 def deriv_side(f, x, h):
     return (f(x + h) - f(x)) / h
 
@@ -17,9 +26,12 @@ def deriv (f, x, h, n):
 
 if __name__ == '__main__':
     h = 10**-3
-
     print(deriv(lambda x: np.sin(x), 0, h, 1))
     print(deriv(lambda x: np.sin(x), 0, h, 2))
     print(deriv(lambda x: np.sin(x), 0, h, 3))
     print(deriv(lambda x: np.sin(x), 0, h, 4))
 
+n = 0
+while fibo(n) < 10**6:
+    n += 1
+print(fibo(n-1))
