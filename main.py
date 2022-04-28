9b9e32e2641abbd3423b713b2df50672bc910ccc
Yizhi Jiao <yizhi_jiao@ucsb.edu>
2022-04-27 20:49:17 -0700

    Section 4

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
-import numpy as np
+# import numpy as np
+#
+#
+# def fibo(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fibo(n-1)+fibo(n-2)
+#
+#
+# def deriv_side(f, x, h):
+#     return (f(x + h) - f(x)) / h
+#
+#
+# def deriv_center(f, x, h):
+#     return (f(x + h) - f(x - h)) / (2 * h)
+#
+#
+# def deriv (f, x, h, n):
+#     if n == 1:
+#         return deriv_center(f, x, h)
+#     return (deriv(f, x+h, h, n-1)-deriv(f, x-h, h, n-1))/(2*h)
+#
+#
+# if __name__ == '__main__':
+#     h = 10**-3
+#     print(deriv(lambda x: np.sin(x), 0, h, 1))
+#     print(deriv(lambda x: np.sin(x), 0, h, 2))
+#     print(deriv(lambda x: np.sin(x), 0, h, 3))
+#     print(deriv(lambda x: np.sin(x), 0, h, 4))
+#
+# n = 0
+# while fibo(n) < 10**6:
+#     n += 1
+# print(fibo(n-1))
 
 
-def fibo(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibo(n-1)+fibo(n-2)
+import sympy as s
 
+from sympy.abc import x, y
 
-def deriv_side(f, x, h):
-    return (f(x + h) - f(x)) / h
+print(s.integrate((1 + x ** 2 + y ** 2)**(-3 / 2), (x, 0, 1), (y, 0, 1)))
 
-
-def deriv_center(f, x, h):
-    return (f(x + h) - f(x - h)) / (2 * h)
-
-
-def deriv (f, x, h, n):
-    if n == 1:
-        return deriv_center(f, x, h)
-    return (deriv(f, x+h, h, n-1)-deriv(f, x-h, h, n-1))/(2*h)
-
-
-if __name__ == '__main__':
-    h = 10**-3
-    print(deriv(lambda x: np.sin(x), 0, h, 1))
-    print(deriv(lambda x: np.sin(x), 0, h, 2))
-    print(deriv(lambda x: np.sin(x), 0, h, 3))
-    print(deriv(lambda x: np.sin(x), 0, h, 4))
-
-n = 0
-while fibo(n) < 10**6:
-    n += 1
-print(fibo(n-1))
